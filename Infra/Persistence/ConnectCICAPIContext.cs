8aae3475ab0bf3cbd90b7102a494cdb1085fd0dc
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-04-09 20:25:45 -0300

    add dbcontext

---
+++ Infra/Persistence/ConnectCICAPIContext.cs
@@ -0,0 +1,23 @@
+﻿namespace connect_cic_api.Infra.Persisence;
+
+using Microsoft.EntityFrameworkCore;
+
+public class ConnectCICAPIContext : DbContext
+{
+
+    public ConnectCICAPIContext(DbContextOptions<ConnectCICAPIContext> options) : base(options)
+      {
+        //Database.EnsureDeleted();
+        Database.EnsureCreated();
+      }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+        #region SqLite ConnectionString
+        var StringConnection = "Data Source=uesc_courses.db";
+        optionsBuilder.UseSqlite(StringConnection)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        #endregion
+    }
+}
