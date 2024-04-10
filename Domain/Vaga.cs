4eb9e2bd79b367810ba79ebb39c177be4d4296ce
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-04-09 21:15:09 -0300

    add vagas domain

--- Domain/Vaga.cs
+++ Domain/Vaga.cs
@@ -2,5 +2,17 @@
 
 public class Vaga
 {
+    public int VagaID { get; set; }
+    public required float Valor { get; set; }
+    public required DateTime DataInicio { get; set; }
+    public required DateTime DataFim { get; set; } 
+
+    public string? Requisitos { get; set; }
+
+    public string? Descricao { get; set; }
+    public required string TituloProjeto { get; set; }
+
+    public required string Status { get; set; }
+
 
 }
