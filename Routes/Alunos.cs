3404a184785dcdcf6bde70c51d47acb7f0b1e9d3
Lavínia Fahning de A <lfassis.cic@uesc.br>
2024-04-15 22:49:53 -0300

    update AlunosRouter

--- Routes/Alunos.cs
+++ Routes/Alunos.cs
@@ -8,7 +8,7 @@ public static class Alunos
     public static void RegisterAlunosEndpoint(this IEndpointRouteBuilder routes){
         
         var AlunosRoutes = routes.MapGroup("/vagas/{id}/Alunos");
-        
+       
         AlunosRoutes.MapGet("", (ConnectCICAPIContext context) => context.Alunos.ToList());
 
         AlunosRoutes.MapPost("", (Aluno alunosrout, ConnectCICAPIContext context) =>
@@ -18,5 +18,28 @@ public static class Alunos
          context.SaveChanges();
          return alunosrout;
         });
+    
+
+    
+         AlunosRoutes.MapPut("/{id}", (int id, Aluno alunosrout, ConnectCICAPIContext context) =>
+        
+        {
+         
+         var AlunosToUpdate = context.Alunos.FirstOrDefault(c => c.AlunoID == id);
+
+         AlunosToUpdate.Nome = alunosrout.Nome;
+         context.SaveChanges();
+         return AlunosToUpdate;
+        });
+
+        AlunosRoutes.MapDelete("/{id}", (int id, ConnectCICAPIContext context) =>
+
+        {
+         var AlunosToDelete = context.Alunos.FirstOrDefault(c => c.AlunoID == id);
+         context.Alunos.Remove(AlunosToDelete);
+         context.SaveChanges();
+         return AlunosToDelete;
+        });
+
     }
 }
