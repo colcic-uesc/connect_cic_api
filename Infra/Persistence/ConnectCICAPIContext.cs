cf242715de1d37cecf3a835d1b7d7b35a42ea894
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-04-28 23:20:39 -0300

    add seed data

--- Infra/Persistence/ConnectCICAPIContext.cs
+++ Infra/Persistence/ConnectCICAPIContext.cs
@@ -29,6 +29,8 @@ public class ConnectCICAPIContext : DbContext
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
       {
+
+            # region Enities Configuration
             modelBuilder.Entity<Usuario>().ToTable("Usuarios");
             modelBuilder.Entity<Usuario>().HasKey(c => c.UsuarioID);
             modelBuilder.Entity<Usuario>()
@@ -49,6 +51,7 @@ public class ConnectCICAPIContext : DbContext
             modelBuilder.Entity<Aluno>().HasKey(a => a.AlunoID);
 
 
+
             modelBuilder.Entity<Professor>().ToTable("Professores");
             modelBuilder.Entity<Professor>().HasKey(p => p.ProfessorID);
             
@@ -58,6 +61,100 @@ public class ConnectCICAPIContext : DbContext
             modelBuilder.Entity<Vaga>().HasOne(v => v.Professor).WithMany(p => p.Vagas).HasForeignKey(v => v.ProfessorID);
             modelBuilder.Entity<Vaga>().HasOne(v => v.TipoVaga).WithMany(t => t.Vagas).HasForeignKey(v => v.TipoVagaID);
             modelBuilder.Entity<Vaga>().HasMany(v => v.Alunos).WithMany(a => a.Vagas);
+            #endregion
 
+
+            # region seed data
+            modelBuilder.Entity<TipoVaga>().HasData(
+                new TipoVaga { VagaTipoID = 1, Nome = "Estágio" },
+                new TipoVaga { VagaTipoID = 2, Nome = "Iniciação Cientifica" },
+                new TipoVaga { VagaTipoID = 3, Nome = "Iniciação a docencia" }
+            );
+
+            modelBuilder.Entity<Aluno>().HasData(
+                new Aluno { AlunoID = 1, 
+                            Nome = "Everaldina Barbosa", 
+                            EmailContato = "everaldina@gmail.com",
+                            Curso = "Ciencia da Computação",
+                            CRAA = 9f,
+                            Status = "Cursando",
+                },
+                new Aluno { AlunoID = 2, 
+                            Nome = "Lavinia", 
+                            EmailContato = "lavinia@gmail.com",
+                            Curso = "Ciencia da Computação",
+                            CRAA = 9f,
+                            Status = "Cursando",
+                },
+                new Aluno { AlunoID = 3, 
+                            Nome = "ana cristina", 
+                            EmailContato = "ana_cristina@gmail.com",
+                            Curso = "Ciencia da Computação",
+                            CRAA = 9f,
+                            Status = "Cursando",
+                },
+                new Aluno { AlunoID = 4, 
+                            Nome = "gabie", 
+                            EmailContato = "gabie@gmail.com",
+                            Curso = "Ciencia da Computação",
+                            CRAA = 9f,
+                            Status = "Cursando",
+                }
+            );
+
+            modelBuilder.Entity<Professor>().HasData(
+                new Professor { ProfessorID = 1, 
+                                Nome = "Helder", 
+                                EmailContato = "helder@uesc.com",
+                                Departamento = "DCET",
+                },
+                new Professor { ProfessorID = 2, 
+                                Nome = "Martha", 
+                                EmailContato = "martinha@uesc.com",
+                                Departamento = "DCET",
+                },
+                new Professor { ProfessorID = 3, 
+                                Nome = "Bravo", 
+                                EmailContato = "bravo@uesc.com",
+                                Departamento = "DCET",
+                }
+                );
+
+            modelBuilder.Entity<Vaga>().HasData(
+                new Vaga { VagaID = 1, 
+                            Valor = 1000f, 
+                            DataInicio = DateTime.Now, 
+                            DataFim = DateTime.Now.AddDays(30), 
+                            Requisitos = "Conhecimento em Java", 
+                            Descricao = "Desenvolvimento de aplicação web", 
+                            TituloProjeto = "Sistema de gerenciamento de vendas", 
+                            Status = "Aberta", 
+                            ProfessorID = 1, 
+                            TipoVagaID = 1
+                },
+                new Vaga { VagaID = 2, 
+                            Valor = 1000f, 
+                            DataInicio = DateTime.Now, 
+                            DataFim = DateTime.Now.AddDays(30), 
+                            Requisitos = "Conhecimento em Java", 
+                            Descricao = "Desenvolvimento de aplicação web", 
+                            TituloProjeto = "Sistema de gerenciamento de vendas", 
+                            Status = "Aberta", 
+                            ProfessorID = 2, 
+                            TipoVagaID = 1
+                },
+                new Vaga { VagaID = 3, 
+                            Valor = 1000f, 
+                            DataInicio = DateTime.Now, 
+                            DataFim = DateTime.Now.AddDays(30), 
+                            Requisitos = "Conhecimento em Java", 
+                            Descricao = "Desenvolvimento de aplicação web", 
+                            TituloProjeto = "Sistema de gerenciamento de vendas", 
+                            Status = "Aberta", 
+                            ProfessorID = 3, 
+                            TipoVagaID = 2
+                }
+            );
+            # endregion
       }
 }
